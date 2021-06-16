b61c9f3a4e22b695e923476b162dd87823632d11
Arsalan <airavani2018@ada.edu.az>
2021-06-16 21:02:28 +0200

    clean code

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,9 @@
 // HomeKit setup and loop
 //==============================
 
+// access your HomeKit characteristics defined in my_accessory.c
+extern "C" homekit_server_config_t config;
+
 static uint32_t next_heap_millis = 0;
 
 #define PIN_SWITCH 2
@@ -38,9 +41,29 @@ static uint32_t next_heap_millis = 0;
 void my_homekit_setup() {
   pinMode(PIN_SWITCH, OUTPUT);
   digitalWrite(PIN_SWITCH, HIGH);
+
+  // Add the .setter function to get the switch-event sent from iOS Home APP.
+  // The .setter should be added before arduino_homekit_setup.
+  // HomeKit sever uses the .setter_ex internally, see homekit_accessories_init
+  // function. Maybe this is a legacy design issue in the original esp-homekit
+  // library, and I have no reason to modify this "feature".
+
+  arduino_homekit_setup(&config);
+
+  // report the switch value to HomeKit if it is changed (e.g. by a physical
+  // button) bool switch_is_on = true/false; cha_switch_on.value.bool_value =
+  // switch_is_on; homekit_characteristic_notify(&cha_switch_on,
+  // cha_switch_on.value);
 }
 
-void my_homekit_loop() {
+void setup() {
+  Serial.begin(115200);
+  wifi_connect(); // in wifi_info.h
+  // homekit_storage_reset(); // to remove the previous HomeKit pairing storage
+  my_homekit_setup();
+}
+
+void loop() {
   arduino_homekit_loop();
   const uint32_t t = millis();
   if (t > next_heap_millis) {
@@ -49,15 +72,6 @@ void my_homekit_loop() {
     LOG_D("Free heap: %d, HomeKit clients: %d", ESP.getFreeHeap(),
           arduino_homekit_connected_clients_count());
   }
-}
-void setup() {
-  Serial.begin(115200);
-  wifi_connect(); // in wifi_info.h
-  // homekit_storage_reset(); // to remove the previous HomeKit pairing storage 
-  my_homekit_setup();
-}
 
-void loop() {
-  my_homekit_loop();
   delay(10);
 }
